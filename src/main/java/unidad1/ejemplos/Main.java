66fc6a3b012c5276880ba0babcdb563bb2f5e830
Julio Pedro López Bayón <juliolb@educastur.org>
2026-09-24 20:10:39 +0200

    Actualización

--- src/main/java/unidad1/ejemplos/Main.java
+++ src/main/java/unidad1/ejemplos/Main.java
@@ -1,10 +1,29 @@
 package unidad1.ejemplos;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a = 10;
+		System.out.println(a);
+		{
+			int b = 30;
+			;;;;;;;;;;;
+			System.out.println(b);
+			a = b * 20 - a++;
+			b++;
+			{}
+			unMetodo();
+			new Random();
+			System.out.println(a > (b -= 10));
+		}
+		
+	}
+	
+	static int unMetodo() {
+		System.out.println("Hola Mundo!");
+		return 10;
 	}
 
 }
